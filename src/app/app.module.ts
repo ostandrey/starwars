e84d8ca02ebf016a6440798b1ace754d7dbbbed8
Andrew Ostapov <ost.and.g@gmail.com>
2020-10-13 23:14:53 +0300

    initial commit. Add 3 directories with components, services and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { StarshipsComponent } from './starships/starships.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    HeroesComponent,
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
